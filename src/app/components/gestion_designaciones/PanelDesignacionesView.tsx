2e277e46d92f390a2991bdfbe26263fb8a9fe932
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-07-21 16:42:04 +0200

    Bugfix

--- src/app/components/gestion_designaciones/PanelDesignacionesView.tsx
+++ src/app/components/gestion_designaciones/PanelDesignacionesView.tsx
@@ -387,13 +387,13 @@ const DesignacionesView = () => {
   ) => {
     const arbitrosDisponibles = obtenerArbitrosDisponibles(partido.fecha, partido.hora);
 
-    const arbitrosAsignados = Object.keys(designaciones[partido.id] || {}).map(
-      (key) => designaciones[partido.id]?.[key as keyof Designacion]?.nombre
-    );
+    const asignadosOtros = Object.entries(designaciones[partido.id] || {})
+  .filter(([key]) => key !== arbitro) 
+  .map(([, value]) => value?.nombre);
 
-    const arbitrosDisponiblesFiltrados = arbitrosDisponibles.filter(
-      (usuario) => !arbitrosAsignados.includes(usuario.nombre)
-    );
+  const arbitrosDisponiblesFiltrados = arbitrosDisponibles.filter(
+    (usuario) => !asignadosOtros.includes(usuario.nombre)
+  );
 
     const seleccionado = designaciones[partido.id]?.[arbitro];
 
